342abd9a177119f87867a1991018bd0e73753b08
moroshenko <gdgovno@gmail.com>
2019-08-05 17:43:35 +0300

    f

---
+++ mine2d/index.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#0033ff";
+ctx.fill();
+ctx.closePath();
